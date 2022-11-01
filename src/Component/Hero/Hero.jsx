9ff0ddc4b0895689e69a6dee7891af8064cd7c66
Tanvir Ahmed <tanvir.be.global@gmail.com>
2022-11-01 21:07:38 +0600

    added hero section right side 3d modle

--- src/Component/Hero/Hero.jsx
+++ src/Component/Hero/Hero.jsx
@@ -6,22 +6,29 @@ import {
   FaGithubSquare,
   FaEnvelope,
 } from "react-icons/fa";
+import threeDmodle from "../../ThreeDmodle/model.obj.glb";
 
 const Hero = () => {
   return (
     <section className="bg-[#F0F2F5] bg-pattern ">
       <div className="bg-img">
         <div className="container mx-auto px-1 md:px-2 flex min-h-[calc(100vh-56px)]">
-          <div className="w-[40%] flex items-center mb-40">
+          {/* left side */}
+          <div className="w-[45%] flex items-center">
             <div className="ml-20">
               <h3 className="text-3xl text-purple-600 font-semibold mb-2">
                 HELLO, I'M
               </h3>
-              <h1 className="text-5xl text-gray-800 font-medium mb-2">
+              <h1 className="text-5xl text-gray-800 font-semibold mb-2">
                 TANVIR AHMED
               </h1>
-              <h5 className="text-gray-600 text-lg font-light">
-                A MARN Web Develope and Freelancer
+              <h5 className="text-gray-700 text-lg font-normal text-justify">
+                I am a web developer with more than 1 year plus of experience
+                creating a website. My website design is good. I am good at time
+                management and love to take on new challenges. I have a mindset
+                of maintaining a good relationship with clients. I have
+                experience working with a team, I'm always ready to learn any
+                new technology
               </h5>
 
               <ul className="flex mt-6">
@@ -56,7 +63,18 @@ const Hero = () => {
               </button>
             </div>
           </div>
-          <div className="w-[60%]"></div>
+          {/* right side  */}
+          <div className="w-[55%]">
+            <div className="h-full">
+              <model-viewer
+                className="w-full h-full mx-auto border-2"
+                alt="Tanvir Ahmed 3D model"
+                src={threeDmodle}
+                camera-controls
+                touch-action="pan-y"
+              ></model-viewer>
+            </div>
+          </div>
         </div>
       </div>
     </section>
